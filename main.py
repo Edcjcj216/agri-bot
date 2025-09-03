564e61cff9d5b9f8a17257f45c8bb2799d1d9d5d
Edcjcj216 <quocvinh9a3lt@gmail.com>
2025-09-03 14:00:20 +0700

    Update PowerShell test script for Render → DEMO telemetry

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ except Exception:
     ZoneInfo = None
 
 # ============== CONFIG =================
-TB_DEMO_TOKEN = os.getenv("TB_DEMO_TOKEN", "N0wYjbpcm7wCw7YnSCiJ")
+TB_DEMO_TOKEN = os.getenv("TB_DEMO_TOKEN", "DEOAyARAvPbZkHKFVJQa")
 TB_DEVICE_URL = f"https://thingsboard.cloud/api/v1/{TB_DEMO_TOKEN}/telemetry"
 
 LAT = float(os.getenv("LAT", "10.79"))
